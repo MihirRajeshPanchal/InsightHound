89c1ea5b094ffcdb1715753a053a87eead11aa44
siro844 <srinathreddy239@gmail.com>
2024-11-23 10:55:42 +0530

    include router

--- aihounds/app.py
+++ aihounds/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from aihounds.endpoints import keywords
-
+from aihounds.endpoints.crunchbase import crunchbase_router 
 app = FastAPI()
 
 app.include_router(keywords.router)
+app.include_router(crunchbase_router)
 
 @app.get("/")
 def root():
