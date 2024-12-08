9cc06d4ccb1191fe5e6dd162739c557ceffb866d
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2024-12-09 01:32:14 +0530

    fix: change stream to invoke in generate_insight for improved functionality

--- aihounds/services/insights.py
+++ aihounds/services/insights.py
@@ -8,7 +8,7 @@ from aihounds.models.insights import Insight
 def generate_insight(context: str):
     chain = INSIGHT_PROMPT | openai_llm | OutputFixingParser.from_llm(parser=JsonOutputParser(pydantic_object=Insight), llm=openai_llm)
     
-    result = chain.stream({"context": context})
+    result = chain.invoke({"context": context})
     print(result)
     
     insight = result.get("insight", [])
