4c9c58a36e9da6e83b75ad24d74a85825c73260e
Varad Prabhu <varadprabhu111@gmail.com>
2024-12-09 22:59:32 +0530

    fix: unipile id

--- aihounds/endpoints/outreach.py
+++ aihounds/endpoints/outreach.py
@@ -1,34 +1,39 @@
 from typing import Optional
-from aihounds.services.outreach import LinkedinOutreach,get_rivals_test
-from fastapi import APIRouter,Request
+from aihounds.services.outreach import LinkedinOutreach, get_rivals_test
+from fastapi import APIRouter, Request
 from pydantic import BaseModel
+
 outreach_router = APIRouter()
 
-class LinkedinRequest(BaseModel):
-    user_id:str
-    linkedin_urls:list[str]
-    message: Optional[str] = None
-    domain:str
 
-linkedin_outreach=LinkedinOutreach()
+class LinkedinRequest(BaseModel):
+    user_id: str
+    linkedin_urls: list[str]
+    message: Optional[str] = None
+    domain: str
+
+
+linkedin_outreach = LinkedinOutreach()
+
 
 @outreach_router.post("/generate_linkedin")
-async def generate_message(request:LinkedinRequest):
-    response= linkedin_outreach.generate_message(request.user_id,request.domain)
-    return {"detail":response}
+async def generate_message(request: LinkedinRequest):
+    response = linkedin_outreach.generate_message(request.user_id, request.domain)
+    return {"detail": response}
+
 
 @outreach_router.post("/send_linkedin")
-async def send_linkedin_message(request:LinkedinRequest):
-    response= linkedin_outreach.send_message_to_all('5On_vtxqSiGIsXRrOfmf1g',request.message,request.linkedin_urls)
-    return {"detail":response}
-
+async def send_linkedin_message(request: LinkedinRequest):
+    response = linkedin_outreach.send_message_to_all(
+        "Axq_BT2xSrilkSRtXGW8AQ", request.message, request.linkedin_urls
+    )
+    return {"detail": response}
 
 
 @outreach_router.post("/get_rivals")
-async def get_company_profile(data:Request):
-    data=await data.json()
-    response= get_rivals_test(data['locations'],data['num_employees_ranges'],data.get('keyword_tags',None))
-    return {"detail":response}
-
-
-
+async def get_company_profile(data: Request):
+    data = await data.json()
+    response = get_rivals_test(
+        data["locations"], data["num_employees_ranges"], data.get("keyword_tags", None)
+    )
+    return {"detail": response}
