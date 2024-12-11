83b97dfbdd9b604a51653171f648f86164815a77
milinD537 <milindpithadia22@gmail.com>
2024-12-11 09:26:46 +0530

    changed CompetitorMapping & ProductCards button handling to use states

--- frontend/src/components/chat/competitor-mapping/index.tsx
+++ frontend/src/components/chat/competitor-mapping/index.tsx
@@ -5,6 +5,13 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { CompanyProfile } from "@/lib/types/chat"
 import useSelfCompany from "@/hooks/use-self"
+import { useState } from "react"
+
+const grid: Record<number, string> = {
+	1: "grid-cols-[1fr_repeat(2,0fr)]",
+	2: "grid-cols-[0fr_1fr_0fr]",
+	3: "grid-cols-[repeat(2,0fr)_1fr]",
+}
 
 export default function CompetitorMapping({
 	// userData,
@@ -13,16 +20,34 @@ export default function CompetitorMapping({
 	// userData: CompanyData
 	data: CompanyProfile[]
 }) {
+	const [selected, setSelected] = useState(0)
 	const { data: userData } = useSelfCompany()
 	const selfData = userData?.data || data[0]
 
 	return (
 		<section>
-			<div className="compareLtoR | group | overflow-hidden p-2 grid gap-0 grid-cols-[0fr_1fr] peer-checked:grid-cols-[0fr_1fr] has-[.compCard-company:checked]:gap-4 has-[.compCard-company:checked]:grid-cols-[1fr_1fr] transition-[grid-template-columns,gap] duration-500">
+			<div
+				// className="compareLtoR | group | overflow-hidden p-2 grid gap-0 grid-cols-[0fr_1fr] peer-checked:grid-cols-[0fr_1fr] has-[.compCard-company:checked]:gap-4 has-[.compCard-company:checked]:grid-cols-[1fr_1fr] transition-[grid-template-columns,gap] duration-500"
+				className={`compareLtoR | group | overflow-hidden p-2 grid gap-0 transition-[grid-template-columns,gap] duration-500 ${
+					selected !== 0
+						? "grid-cols-[1fr_1fr] gap-4"
+						: "grid-cols-[0fr_1fr]"
+				}`}
+			>
 				{selfData && (
 					<div className="overflow-hidden">
-						<div className="competitorCard-company w-max group-has-[.compCard-company:checked]:w-full [interpolate-size:allow-keywords] transition-[width] duration-500">
-							<button className="bg-foreground text-background font-medium rounded-md grid place-items-center w-full my-4">
+						<div
+							// className="competitorCard-company w-max group-has-[.compCard-company:checked]:w-full [interpolate-size:allow-keywords] transition-[width] duration-500"
+							className={`competitorCard ${
+								selected ? "w-full" : "w-max "
+							} [interpolate-size:allow-keywords] transition-[width] duration-500`}
+						>
+							<button
+								onClick={() => {
+									setSelected(0)
+								}}
+								className="bg-foreground text-background font-medium rounded-md grid place-items-center w-full my-4"
+							>
 								<label
 									htmlFor="compareBack-company"
 									className="w-full h-full px-3 py-1.5"
@@ -32,83 +57,46 @@ export default function CompetitorMapping({
 							</button>
 							<CompanyCard company={selfData} />
 
-							<input
+							{/* <input
 								type="radio"
 								name="compCompare-company"
 								id="compareBack-company"
 								className="peer hidden"
-							/>
+							/> */}
 						</div>
 					</div>
 				)}
-				<div className="grid gap-4 grid-cols-[repeat(3,1fr)] has-[.compCard-company:checked]:gap-0 has-[#compCompareOne-company:checked]:grid-cols-[1fr_0fr_0fr] has-[#compCompareTwo-company:checked]:grid-cols-[0fr_1fr_0fr] has-[#compCompareThree-company:checked]:grid-cols-[0fr_0fr_1fr] transition-[grid-template-columns,gap] duration-500">
-					{data[0] && (
-						<div className="overflow-hidden">
-							<div className="competitorCard-company">
-								<label
-									htmlFor="compCompareOne-company"
+				<div
+					// className="grid gap-4 grid-cols-[repeat(3,1fr)] has-[.compCard-company:checked]:gap-0 has-[#compCompareOne-company:checked]:grid-cols-[1fr_0fr_0fr] has-[#compCompareTwo-company:checked]:grid-cols-[0fr_1fr_0fr] has-[#compCompareThree-company:checked]:grid-cols-[0fr_0fr_1fr] transition-[grid-template-columns,gap] duration-500"
+					className={`grid ${
+						selected !== 0
+							? "gap-0 " + grid[selected]
+							: "gap-4 grid-cols-[repeat(3,1fr)]"
+					} transition-[grid-template-columns,gap] duration-500`}
+				>
+					{data?.slice(1).map((item, i) => (
+						<div key={i} className="overflow-hidden">
+							<div
+								// className="competitorCard-company"
+								className={`competitorCard transition-[width] [interpolate-size:allow-keywords] duration-500  ${
+									selected !== 0 && selected !== i + 1
+										? "w-max"
+										: "w-full"
+								}`}
+							>
+								<button
+									onClick={() => setSelected(i + 1)}
 									className={cn(
 										"bg-background my-4 w-full",
-										buttonVariants({ variant: "default" }),
+										buttonVariants({ variant: "default" })
 									)}
 								>
 									Compare
-								</label>
-								<CompanyCard company={data[0]} />
-								<input
-									type="radio"
-									name="compCompare-company"
-									id="compCompareOne-company"
-									className="compCard-company | hidden"
-								/>
+								</button>
+								<CompanyCard company={item} />
 							</div>
 						</div>
-					)}
-					{data[1] && (
-						<div className="overflow-hidden">
-							<div className="competitorCard-company">
-								<label
-									htmlFor="compCompareTwo-company"
-									className={cn(
-										"bg-background my-4 w-full",
-										buttonVariants({ variant: "default" }),
-									)}
-								>
-									Compare
-								</label>
-								<CompanyCard company={data[1]} />
-								<input
-									type="radio"
-									name="compCompare-company"
-									id="compCompareTwo-company"
-									className="compCard-company | hidden"
-								/>
-							</div>
-						</div>
-					)}
-					{data[2] && (
-						<div className="overflow-hidden">
-							<div className="competitorCard-company">
-								<label
-									htmlFor="compCompareThree-company"
-									className={cn(
-										"bg-background my-4 w-full",
-										buttonVariants({ variant: "default" }),
-									)}
-								>
-									Compare
-								</label>
-								<CompanyCard company={data[2]} />
-
-								<input
-									type="radio"
-									name="compCompare-company"
-									id="compCompareThree-company"
-									className="compCard-company | hidden"
-								/>
-							</div>
-						</div>
-					)}
+					))}
 				</div>
 			</div>
 		</section>
--- frontend/src/components/chat/product-comparison/cards.tsx
+++ frontend/src/components/chat/product-comparison/cards.tsx
@@ -1,25 +1,49 @@
 import { Products } from "@/lib/types/api"
-import React from "react"
+import React, { useState } from "react"
 import ProductCard from "./card"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 interface CardProps {
 	data: Products
 }
+
+const grid: Record<number, string> = {
+	1: "grid-cols-[1fr_repeat(2,0fr)]",
+	2: "grid-cols-[0fr_1fr_0fr]",
+	3: "grid-cols-[repeat(2,0fr)_1fr]",
+}
+
 export default function ProductCards({ data }: CardProps) {
+	const [selected, setSelected] = useState(0)
+
 	return (
 		<section
 			id="product-comparison"
 			className={cn(
-				"grow transition-all delay-500 duration-500 min-h-[70vh]",
-				data ? "opacity-100" : "opacity-0",
+				"grow transition-all delay-500 duration-500 ~min-h-[70vh]",
+				data ? "opacity-100" : "opacity-0"
 			)}
 		>
-			<div className="compareLtoR | group | overflow-hidden p-2 grid gap-0 grid-cols-[0fr_1fr] peer-checked:grid-cols-[0fr_1fr] has-[.compCard:checked]:gap-4 has-[.compCard:checked]:grid-cols-[1fr_1fr] transition-[grid-template-columns,gap] duration-500">
+			<div
+				className={`compareLtoR | group | overflow-hidden p-2 grid gap-0 transition-[grid-template-columns,gap] duration-500 ${
+					selected !== 0
+						? "grid-cols-[1fr_1fr] gap-4"
+						: "grid-cols-[0fr_1fr]"
+				}`}
+			>
 				{data?.[0] && (
 					<div className="overflow-hidden">
-						<div className="competitorCard w-max group-has-[.compCard:checked]:w-full [interpolate-size:allow-keywords] transition-[width] duration-500">
-							<button className="bg-foreground text-background font-medium rounded-md grid place-items-center w-full my-4">
+						<div
+							className={`competitorCard ${
+								selected ? "w-full" : "w-max "
+							} [interpolate-size:allow-keywords] transition-[width] duration-500`}
+						>
+							<button
+								onClick={() => {
+									setSelected(0)
+								}}
+								className="bg-foreground text-background font-medium rounded-md grid place-items-center w-full my-4"
+							>
 								<label
 									htmlFor="compareBack"
 									className="w-full h-full px-3 py-1.5"
@@ -29,83 +53,45 @@ export default function ProductCards({ data }: CardProps) {
 							</button>
 							<ProductCard data={data[0]} />
 
-							<input
+							{/* <input
 								type="radio"
 								name="compCompare"
 								id="compareBack"
 								className="peer hidden"
-							/>
+							/> */}
 						</div>
 					</div>
 				)}
-				<div className="grid gap-4 grid-cols-[repeat(3,1fr)] has-[.compCard:checked]:gap-0 has-[#compCompareOne:checked]:grid-cols-[1fr_0fr_0fr] has-[#compCompareTwo:checked]:grid-cols-[0fr_1fr_0fr] has-[#compCompareThree:checked]:grid-cols-[0fr_0fr_1fr] transition-[grid-template-columns,gap] duration-500">
-					{data?.[1] && (
-						<div className="overflow-hidden">
-							<div className="competitorCard">
-								<label
-									htmlFor="compCompareOne"
-									className={cn(
-										"bg-background my-4 w-full",
-										buttonVariants({ variant: "default" }),
-									)}
-								>
-									Compare
-								</label>
-								<ProductCard data={data[1]} />
-								<input
-									type="radio"
-									name="compCompare"
-									id="compCompareOne"
-									className="compCard | hidden"
-								/>
-							</div>
-						</div>
-					)}
-					{data?.[2] && (
-						<div className="overflow-hidden">
-							<div className="competitorCard">
-								<label
-									htmlFor="compCompareTwo"
-									className={cn(
-										"bg-background my-4 w-full",
-										buttonVariants({ variant: "default" }),
-									)}
-								>
-									Compare
-								</label>
-								<ProductCard data={data[2]} />
-								<input
-									type="radio"
-									name="compCompare"
-									id="compCompareTwo"
-									className="compCard | hidden"
-								/>
-							</div>
-						</div>
-					)}
-					{data?.[3] && (
-						<div className="overflow-hidden">
-							<div className="competitorCard">
-								<label
-									htmlFor="compCompareThree"
-									className={cn(
-										"bg-background my-4 w-full",
-										buttonVariants({ variant: "default" }),
-									)}
-								>
-									Compare
-								</label>
-								<ProductCard data={data[3]} />
 
-								<input
-									type="radio"
-									name="compCompare"
-									id="compCompareThree"
-									className="compCard | hidden"
-								/>
+				<div
+					className={`grid ${
+						selected !== 0
+							? "gap-0 " + grid[selected]
+							: "gap-4 grid-cols-[repeat(3,1fr)]"
+					} transition-[grid-template-columns,gap] duration-500`}
+				>
+					{data?.slice(1).map((item, i) => (
+						<div key={i} className="overflow-hidden">
+							<div
+								className={`competitorCard transition-[width] [interpolate-size:allow-keywords] duration-500  ${
+									selected !== 0 && selected !== i + 1
+										? "w-max"
+										: "w-full"
+								}`}
+							>
+								<button
+									onClick={() => setSelected(i + 1)}
+									className={cn(
+										"bg-background my-4 w-full",
+										buttonVariants({ variant: "default" })
+									)}
+								>
+									Compare
+								</button>
+								<ProductCard data={item} />
 							</div>
 						</div>
-					)}
+					))}
 				</div>
 			</div>
 		</section>
